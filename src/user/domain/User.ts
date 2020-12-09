433f2111c117aacd6138522d1471ed33d6adcb5e
shin yeong hyeon <den.shin.dev@gmail.com>
2020-12-09 14:47:54 +0900

    Implement User Base Domain

---
+++ src/user/domain/User.ts
@@ -0,0 +1,31 @@
+import { Result } from '../../shared/core/Result';
+import { AggregateRoot } from '../../shared/domain/AggregateRoot';
+import { UniqueEntityId } from '../../shared/domain/UniqueEntityId';
+import { UserName } from './UserName';
+
+interface UserProps {
+  userName: UserName;
+  createdAt: Date;
+}
+
+interface UserNewProps {
+  userName: UserName;
+}
+
+export class User extends AggregateRoot<UserProps> {
+  static create(props: UserProps, id?: UniqueEntityId): Result<User> {
+    return Result.ok(new User(props, id));
+  }
+
+  static createNew(props: UserNewProps): Result<User> {
+    return User.create({ ...props, createdAt: new Date() });
+  }
+
+  private constructor(props: UserProps, id?: UniqueEntityId) {
+    super(props, id);
+  }
+
+  get name(): UserName {
+    return this.props.userName;
+  }
+}
