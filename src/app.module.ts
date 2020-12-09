591b79b3cf544e75e3189e109834ab83df710fed
shin yeong hyeon <den.shin.dev@gmail.com>
2020-12-09 15:41:45 +0900

    Config mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { UserModule } from './user/user.module';
@@ -6,10 +9,34 @@ import { HealthModule } from './health/HealthModule';
 
 @Module({
   imports: [
-    HealthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_EV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('prod', 'dev', 'test'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV === 'dev',
+      entities: [],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    HealthModule,
     UserModule,
   ],
   controllers: [],
