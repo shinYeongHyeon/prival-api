3b3a04c38e8d1191762b720f2575e51d3fd618bf
shin yeong hyeon <den.shin.dev@gmail.com>
2020-12-10 16:04:55 +0900

    - Implement User Id, Pwd - pwd hash - delete modelMapper

--- src/user/domain/User.spec.ts
+++ src/user/domain/User.spec.ts
@@ -1,17 +1,22 @@
 import { Result } from '../../shared/core/Result';
 import { UserName } from './UserName';
 import { User } from './User';
+import { UserEmail } from './UserEmail';
 
 describe('User', () => {
   const USER_NAME = '신영현';
+  const USER_EMAIL = 'dev.den.shin@gmail.com';
 
   let userOrError: Result<User>;
   let userName: UserName;
+  let userEmail: UserEmail;
 
   beforeAll(() => {
     userName = UserName.create(USER_NAME).value;
+    userEmail = UserEmail.create(USER_EMAIL).value;
     userOrError = User.createNew({
       userName,
+      userEmail,
     });
   });
 
@@ -22,5 +27,7 @@ describe('User', () => {
   it('getter 들이 정상작동하는지', () => {
     expect(userOrError.value.name).toEqual(userName);
     expect(userOrError.value.name.value).toEqual(USER_NAME);
+    expect(userOrError.value.email).toEqual(userEmail);
+    expect(userOrError.value.email.value).toEqual(USER_EMAIL);
   });
 });
