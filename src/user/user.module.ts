5e75f7f165d5a4d379d839f115b4700bc998ccf1
shin yeong hyeon <den.shin.dev@gmail.com>
2020-12-13 17:47:30 +0900

    Implement Calendar Entity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CreateUserUseCase } from './application/CreateUser/CreateUserUseCase';
-import { UserEntity } from './entity/User.entity';
-import { MysqlUserRepository } from './infra/mysql/MysqlUser.repository';
-import { UserResolver } from './resolver/User.resolver';
 import { LoginUseCase } from './application/Login/LoginUseCase';
 import { FindUserUseCase } from './application/FindUser/FindUserUseCase';
 import { EditUserProfileUseCase } from './application/EditUserProfile/EditUserProfileUseCase';
+import { MysqlUserRepository } from './infra/mysql/MysqlUser.repository';
+import { UserEntity } from './entity/User.entity';
+import { UserResolver } from './resolver/User.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
