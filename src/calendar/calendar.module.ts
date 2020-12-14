9b06e6949edc268eaae7c2ecee5762703c0d3f27
shin yeong hyeon <den.shin.dev@gmail.com>
2020-12-14 16:08:22 +0900

    Implement CalendarDefaultResolver

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CalendarEntity } from './entity/Calendar.entity';
 import { MysqlCalendarRepository } from './infra/mysql/MysqlCalendar.repository';
 import { CreateDefaultCalendarUseCase } from './application/CreateDefaultCalendar/CreateDefaultCalendarUseCase';
+import { CalendarResolver } from './resolver/Calendar.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CalendarEntity])],
   providers: [
     CreateDefaultCalendarUseCase,
+    CalendarResolver,
     {
       provide: 'CALENDAR_REPOSITORY',
       useClass: MysqlCalendarRepository,
