23f8ae96136b946e9814d141b614b4d49aab9e69
shin yeong hyeon <den.shin.dev@gmail.com>
2020-12-16 23:02:00 +0900

    Build Init

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,8 +21,6 @@ import { UsersCalendarEntity } from './calendar/entity/UsersCalendar.entity';
 import { CalendarEntity } from './calendar/entity/Calendar.entity';
 import { ScheduleEntity } from './schedule/entity/Schedule.entity';
 
-console.log('build test');
-
 @Module({
   imports: [
     ConfigModule.forRoot({
