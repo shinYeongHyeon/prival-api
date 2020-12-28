a961fa1560a22e8aa31cd94385563a269c145fa8
shinYeongHyeon <den.shin.dev@gmail.com>
2020-12-29 05:20:44 +0900

    세팅 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,7 +40,7 @@ import { ScheduleEntity } from './schedule/entity/Schedule.entity';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: +process.env.DB_HOST,
+      port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
